136a559c7d93f0cf49cfa17e0bd1f19eb9864a55
Marcin Lamberski <marcin.lamberski@gmail.com>
2023-10-20 08:22:36 +0200

    removed useless files

--- src/main/java/codepred/ride/dto/RideResponse.java
+++
@@ -1,7 +0,0 @@
-package codepred.ride.dto;
-
-public record RideResponse(Integer id, String start, String destination, String startDate, String startHour, String driver,
-                           String phoneNumber, String photo, Integer distanceFromStart) {
-
-
-}
--- src/main/java/codepred/ride/dto/SubmitRideRequest.java
+++
@@ -1,9 +0,0 @@
-package codepred.ride.dto;
-
-
-public record SubmitRideRequest(String start, String destination, String startDate , String startHour) {
-
-
-}
-
-
