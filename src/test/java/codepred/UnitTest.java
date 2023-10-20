b7bf6a00c59e83b92d08768624e4a144038f8cc3
Marcin Lamberski <marcin.lamberski@gmail.com>
2023-10-20 08:23:11 +0200

    created unit test config

---
+++ src/test/java/codepred/UnitTest.java
@@ -0,0 +1,8 @@
+package codepred;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public interface UnitTest {
+}
